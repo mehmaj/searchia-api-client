ea87de7eeabb3ccba5118c3f0871593c341e4313
mehmaj <31012967+mehmaj@users.noreply.github.com>
2020-12-10 03:32:42 +0330

    Add files via upload
    some changes

--- searchia-api-client.js
+++ searchia-api-client.js
@@ -1,4 +1,3 @@
-  
 class SearchiaBuilder {
    constructor(options = {}){
         Object.assign(this, {
@@ -13,19 +12,21 @@ class SearchiaBuilder {
      searchiaSearchInputId:'searchia-search-box',
      searciaFrom:0,
      searchiaNRPP:10,
-     runSearchiaOnInputChange:true
+     runSearchiaOnInputChange:true,
+     //do not touch below
+     sISLastReqTimeStamp:0
         }, options);
       if (this.runSearchiaOnInputChange==true) {
          var self=this;
          document.getElementById(this.searchiaSearchInputId).addEventListener('input', function() {
-             self.instantSearch();
+             self.searchiaInstantSearch();
          });
       }
     }
   fillTemplate(templateString, templateVars){
     return new Function("return `"+templateString +"`;").call(templateVars);
   }
-  addToCart(docId){
+  searchiaAddToCart(docId){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("PUT", "https://Searchia.ir/api/cart/index/"+this.searchiaIndexName+"/doc/"+docId);
     xmlhttp.setRequestHeader("Content-type", "application/json");
@@ -33,14 +34,14 @@ class SearchiaBuilder {
     xmlhttp.send();
   }
 
-  click(docId,query,position){
+  searchiaClick(query,docId,position){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("PUT", "https://Searchia.ir/api/click/index/"+this.searchiaIndexName+"/doc/"+docId+"?query="+query+"&position="+position);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.setRequestHeader("APIKEY", this.searchiaAPIKey);
     xmlhttp.send();
   }
-  search(query,from=this.searchiaFrom,nrpp=this.searchiaNRPP){
+  searchiaSearch(query,from=this.searchiaFrom,nrpp=this.searchiaNRPP){
     var self = this;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -68,34 +69,52 @@ class SearchiaBuilder {
     xmlhttp.setRequestHeader("APIKEY", this.searchiaAPIKey);
     xmlhttp.send();
   }
-  
-  instantSearch(query=document.getElementById(this.searchiaSearchInputId).value,nrpp=this.searchiaNRPP){
+  searchiaInstantSearch(query=document.getElementById(this.searchiaSearchInputId).value,nrpp=this.searchiaNRPP){
+    let ts=new Date().getTime();
+    this.sISLastReqTimeStamp=ts;
+    if(!query){
+      try {
+                document.getElementById(this.searchiaInstantSearchResultContainer).innerHTML ="";
+                document.getElementById(this.searchiaTotalHitsContainer).innerHTML ="";
+                document.getElementById(this.searchiaSearchTime).innerHTML = "";
+              }catch(err) {}
+      return
+    }
     var self = this;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          var resObj = JSON.parse(this.responseText);
-         try {
-            document.getElementById(self.searchiaTotalHitsContainer).innerHTML = resObj.entity.totalHits;
-            document.getElementById(self.searchiaSearchTime).innerHTML = resObj.entity.searchTime;
-          }catch(err) {}
-          var resContainer = document.getElementById(self.searchiaInstantSearchResultContainer);
-          resContainer.innerHTML="";
-          var ind=0;
-          for (var elem in resObj.entity.results) {
-            this.orgData=JSON.parse(resObj.entity.results[elem].source);
-            this.orgData["docId"]=resObj.entity.results[elem].documentId;
-            this.orgData["position"]=ind;
-            resContainer.innerHTML+=self.fillTemplate(self.searchiaInstantSearchTemplate,this.orgData);
-            ind++;
-          }
-        
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open("GET", "https://Searchia.ir/api/index/"+this.searchiaIndexName+"?query="+query+"&from="+this.searciaFrom+"&size="+nrpp);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.setRequestHeader("APIKEY", this.searchiaAPIKey);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      let responseObj = xhr.response;
+        if (this.readyState ==4) {
+            if (ts<self.sISLastReqTimeStamp)
+              return;
+            if (this.status == 200) {
+             self.sISLastReqTimeStamp=self.ts;
+             try {
+                document.getElementById(self.searchiaTotalHitsContainer).innerHTML =responseObj.entity.totalHits;
+                document.getElementById(self.searchiaSearchTime).innerHTML = responseObj.entity.searchTime;
+              }catch(err) {}
+              var resContainer = document.getElementById(self.searchiaInstantSearchResultContainer);
+              resContainer.innerHTML="";
+              var ind=0;
+              for (var elem in responseObj.entity.results) {
+                this.orgData=JSON.parse(responseObj.entity.results[elem].source);
+                this.orgData["docId"]=responseObj.entity.results[elem].documentId;
+                this.orgData["position"]=ind;
+                resContainer.innerHTML+=self.fillTemplate(self.searchiaInstantSearchTemplate,this.orgData);
+                ind++;
+              }
+            }
+            else 
+              console.log(responseObj.message);
+            
         }
-      }
-    xmlhttp.open("GET", "https://Searchia.ir/api/index/"+this.searchiaIndexName+"?query="+query+"&from="+this.searciaFrom+"&size="+nrpp);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.setRequestHeader("APIKEY", this.searchiaAPIKey);
-    xmlhttp.send();
+      
+      };
   }
 
   
